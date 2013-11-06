a17ffc36dc2e44d97bac8036f38ed554b3259f30
Joe Ray Gregory <j.r.gregory@gmx.de>
2013-11-06 16:10:34 +0100

    init commit with new features

---
+++ m17PageFolders/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package M17PageFolders
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'nav_m17PageFolders' => 'system/modules/m17PageFolders/templates',
+));
