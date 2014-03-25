bf297831b0ca7aa37dbfeb44d8c22aebe0f85e36
Joe Ray Gregory <j.r.gregory@gmx.de>
2014-03-25 01:03:02 +0100

    And one more

--- TL_ROOT/system/modules/m17PageFolders/config/autoload.php
+++ TL_ROOT/system/modules/m17PageFolders/config/autoload.php
@@ -3,12 +3,21 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2013 Leo Feyer
+ * Copyright (c) 2005-2014 Leo Feyer
  *
  * @package M17PageFolders
  * @link    https://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	'PageM17PageFolder' => 'system/modules/m17PageFolders/PageM17PageFolder.php',
+));
 
 
 /**
